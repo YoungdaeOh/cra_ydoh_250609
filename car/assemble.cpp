d068e646d861db9c1d275b29b4449c6a90e90e18
Youngdae Oh <ydoh84@gmail.com>
2025-06-09 14:31:08 +0900

    Class CarBuilder is added
    Signed-off-by: Youngdae Oh <ydoh84@gmail.com>

--- car/assemble.cpp
+++ car/assemble.cpp
@@ -1,38 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
 
 #define CLEAR_SCREEN "\033[H\033[2J"
 #define DEFAULT_DELAY 800
 
 int stack[10];
-
-void selectCarType(int answer);
-void selectEngine(int answer);
-void selectbrakeSystem(int answer);
-void selectSteeringSystem(int answer);
-void menu_cartype(void);
-void menu_engine(void);
-void menu_brake(void);
-void menu_steering(void);
-void menu_run(void);
-void menu_print(int step);
-int input_handle_cartype(int step, int answer);
-int input_handle_engine(int step, int answer);
-int input_handle_brakesystem(int step, int answer);
-int input_handle_steeringsystem(int step, int answer);
-int input_handle_runtest(int step, int answer);
-int input_handle(int step, int answer);
-bool isValidNumber(char* buf, int* out_answer);
-bool is_exit(char* buf);
-void getInput(char* buf);
-void print_cartype(void);
-void print_engine(void);
-void print_brakesystem(void);
-void print_steeringsystem(void);
-void runProducedCar();
-void testProducedCar();
-void delay(int ms);
 
 enum QuestionType
 {
@@ -70,22 +48,54 @@ enum SteeringSystem
     MOBIS
 };
 
-void delay(int ms)
-{
+
+class CarBuilder {
+private:
+    int stack[10]{};
+    QuestionType step = CarType_Q;
+
+public:
+    void run();
+
+private:
+    void menu_cartype(void);
+    void menu_engine(void);
+    void menu_brake(void);
+    void menu_steering(void);
+    void menu_runtest(void);
+    void menu_print(void);
+    bool is_exit(char* buf);
+    void get_input(char* buf);
+    bool is_valid_number(char* buf, int* out_answer);
+    void delay(int ms);
+    void input_handle_cartype(int answer);
+    void input_handle_engine(int answer);
+    void input_handle_brakesystem(int answer);
+    void input_handle_steeringsystem(int answer);
+    void input_handle_runtest(int answer);
+    void input_handle(int answer);
+    void select_car_type(int answer);
+    void select_engine(int answer);
+    void select_brake_system(int answer);
+    void select_steering_system(int answer);
+    int is_valid_assemble();
+    void print_cartype(void);
+    void print_engine(void);
+    void print_brakesystem(void);
+    void print_steeringsystem(void);
+    void run_produced_car();
+    void test_produced_car();
+};
+
+void CarBuilder::delay(int ms) {
     volatile int sum = 0;
-    for (int i = 0; i < 1000; i++)
-    {
-        for (int j = 0; j < 1000; j++)
-        {
-            for (int t = 0; t < ms; t++)
-            {
+    for (int i = 0; i < 1000; ++i)
+        for (int j = 0; j < 1000; ++j)
+            for (int t = 0; t < ms; ++t)
                 sum++;
-            }
-        }
-    }
 }
 
-void menu_cartype(void)
+void CarBuilder::menu_cartype(void)
 {
     printf("        ______________\n");
     printf("       /|            | \n");
@@ -99,7 +109,7 @@ void menu_cartype(void)
     printf("3. Truck\n");
 }
 
-void menu_engine(void)
+void CarBuilder::menu_engine(void)
 {
     printf("어떤 엔진을 탑재할까요?\n");
     printf("0. 뒤로가기\n");
@@ -109,7 +119,7 @@ void menu_engine(void)
     printf("4. 고장난 엔진\n");
 }
 
-void menu_brake(void)
+void CarBuilder::menu_brake(void)
 {
     printf("어떤 제동장치를 선택할까요?\n");
     printf("0. 뒤로가기\n");
@@ -118,7 +128,7 @@ void menu_brake(void)
     printf("3. BOSCH\n");
 }
 
-void menu_steering(void)
+void CarBuilder::menu_steering(void)
 {
     printf("어떤 조향장치를 선택할까요?\n");
     printf("0. 뒤로가기\n");
@@ -126,7 +136,7 @@ void menu_steering(void)
     printf("2. MOBIS\n");
 }
 
-void menu_run(void)
+void CarBuilder::menu_runtest(void)
 {
     printf("멋진 차량이 완성되었습니다.\n");
     printf("어떤 동작을 할까요?\n");
@@ -135,132 +145,44 @@ void menu_run(void)
     printf("2. Test\n");
 }
 
-void menu_print(int step)
+void CarBuilder::menu_print(void)
 {
     printf(CLEAR_SCREEN);
     printf("===============================\n");
     if (step == CarType_Q)
-    {
         menu_cartype();
-    }
     else if (step == Engine_Q)
-    {
         menu_engine();
-    }
     else if (step == brakeSystem_Q)
-    {
         menu_brake();
-    }
     else if (step == SteeringSystem_Q)
-    {
         menu_steering();
-    }
     else if (step == Run_Test)
-    {
-        menu_run();
-    }
+        menu_runtest();
 }
 
-int input_handle_cartype(int step, int answer)
+bool CarBuilder::is_exit(char* buf)
 {
-    if (!(answer >= 1 && answer <= 3))
+    if (!strcmp(buf, "exit"))
     {
-        printf("ERROR :: 차량 타입은 1 ~ 3 범위만 선택 가능\n");
+        printf("바이바이\n");
+        return true;
     }
-    else {
-        selectCarType(answer);
-        step = Engine_Q;
-    }
-    delay(DEFAULT_DELAY);
-    return step;
+    return false;
 }
 
-int input_handle_engine(int step, int answer)
+void CarBuilder::get_input(char* buf)
 {
-    if (!(answer >= 0 && answer <= 4))
-    {
-        printf("ERROR :: 엔진은 1 ~ 4 범위만 선택 가능\n");
-    }
-    else {
-        selectEngine(answer);
-        step = brakeSystem_Q;
-    }
-    delay(DEFAULT_DELAY);
-    return step;
+    printf("INPUT > ");
+    fgets(buf, sizeof(buf), stdin);
+
+    // 엔터 개행문자 제거
+    char* context = nullptr;
+    strtok_s(buf, "\r", &context);
+    strtok_s(buf, "\n", &context);
 }
 
-int input_handle_brakesystem(int step, int answer)
-{
-    if (!(answer >= 0 && answer <= 3))
-    {
-        printf("ERROR :: 제동장치는 1 ~ 3 범위만 선택 가능\n");
-    }
-    else {
-        selectbrakeSystem(answer);
-        step = SteeringSystem_Q;
-    }
-    delay(DEFAULT_DELAY);
-    return step;
-}
-
-int input_handle_steeringsystem(int step, int answer)
-{
-    if (!(answer >= 0 && answer <= 2))
-    {
-        printf("ERROR :: 조향장치는 1 ~ 2 범위만 선택 가능\n");
-    }
-    else {
-        selectSteeringSystem(answer);
-        step = Run_Test;
-    }
-    delay(DEFAULT_DELAY);
-    return step;
-}
-
-int input_handle_runtest(int step, int answer)
-{
-    // 처음으로 돌아가기
-    if (answer == 0) {
-        step = CarType_Q;
-    }
-    else if (answer == 1)
-    {
-        runProducedCar();
-        delay(2000);
-    }
-    else if (answer == 2)
-    {
-        printf("Test...\n");
-        delay(1500);
-        testProducedCar();
-        delay(2000);
-    }
-    else {
-        printf("ERROR :: Run 또는 Test 중 하나를 선택 필요\n");
-        delay(DEFAULT_DELAY);
-    }
-    return step;
-}
-
-int input_handle(int step, int answer)
-{
-    switch (step) {
-    case CarType_Q:
-        return input_handle_cartype(step, answer);
-    case Engine_Q:
-        return input_handle_engine(step, answer);
-    case brakeSystem_Q:
-        return input_handle_brakesystem(step, answer);
-    case SteeringSystem_Q:
-        return input_handle_steeringsystem(step, answer);
-    case Run_Test:
-        return input_handle_runtest(step, answer);
-    default:
-        return step;
-    }
-}
-
-bool isValidNumber(char* buf, int* out_answer)
+bool CarBuilder::is_valid_number(char* buf, int* out_answer)
 {
     // 숫자로 된 대답인지 확인
     char* checkNumber;
@@ -276,55 +198,104 @@ bool isValidNumber(char* buf, int* out_answer)
     return true;
 }
 
-bool is_exit(char* buf)
+void CarBuilder::input_handle_cartype(int answer)
 {
-    if (!strcmp(buf, "exit"))
+    if (!(answer >= 1 && answer <= 3))
     {
-        printf("바이바이\n");
-        return true;
+        printf("ERROR :: 차량 타입은 1 ~ 3 범위만 선택 가능\n");
     }
-    return false;
+    else {
+        select_car_type(answer);
+        step = Engine_Q;
+    }
+    delay(DEFAULT_DELAY);
 }
 
-void getInput(char* buf)
+void CarBuilder::input_handle_engine(int answer)
 {
-    printf("INPUT > ");
-    fgets(buf, sizeof(buf), stdin);
-
-    // 엔터 개행문자 제거
-    char* context = nullptr;
-    strtok_s(buf, "\r", &context);
-    strtok_s(buf, "\n", &context);
-}
-
-int main()
-{
-    int step = CarType_Q;
-
-    while (1)
+    if (!(answer >= 0 && answer <= 4))
     {
-        char buf[100];
-        int answer;
-        menu_print(step);
-        getInput(buf);
-        if (is_exit(buf))
-            break;
+        printf("ERROR :: 엔진은 1 ~ 4 범위만 선택 가능\n");
+    }
+    else {
+        select_engine(answer);
+        step = brakeSystem_Q;
+    }
+    delay(DEFAULT_DELAY);
+}
 
-        if (isValidNumber(buf, &answer) == false)
-            continue;
+void CarBuilder::input_handle_brakesystem(int answer)
+{
+    if (!(answer >= 0 && answer <= 3))
+    {
+        printf("ERROR :: 제동장치는 1 ~ 3 범위만 선택 가능\n");
+    }
+    else {
+        select_brake_system(answer);
+        step = SteeringSystem_Q;
+    }
+    delay(DEFAULT_DELAY);
+}
 
-        // 이전으로 돌아가기
-        if (step > CarType_Q && step < Run_Test && answer == 0)
-        {
-            step -= 1;
-            continue;
-        }
+void CarBuilder::input_handle_steeringsystem(int answer)
+{
+    if (!(answer >= 0 && answer <= 2))
+    {
+        printf("ERROR :: 조향장치는 1 ~ 2 범위만 선택 가능\n");
+    }
+    else {
+        select_steering_system(answer);
+        step = Run_Test;
+    }
+    delay(DEFAULT_DELAY);
+}
 
-        step = input_handle(step, answer);
+void CarBuilder::input_handle_runtest(int answer)
+{
+    // 처음으로 돌아가기
+    if (answer == 0) {
+        step = CarType_Q;
+    }
+    else if (answer == 1)
+    {
+        run_produced_car();
+        delay(2000);
+    }
+    else if (answer == 2)
+    {
+        printf("Test...\n");
+        delay(1500);
+        test_produced_car();
+        delay(2000);
+    }
+    else {
+        printf("ERROR :: Run 또는 Test 중 하나를 선택 필요\n");
+        delay(DEFAULT_DELAY);
     }
 }
 
-void selectCarType(int answer)
+void CarBuilder::input_handle(int answer)
+{
+    switch (step) {
+    case CarType_Q:
+        input_handle_cartype(answer);
+        break;
+    case Engine_Q:
+        input_handle_engine(answer);
+        break;
+    case brakeSystem_Q:
+        input_handle_brakesystem(answer);
+        break;
+    case SteeringSystem_Q:
+        input_handle_steeringsystem(answer);
+        break;
+    case Run_Test:
+        input_handle_runtest(answer);
+        break;
+    }
+}
+
+void CarBuilder::select_car_type(int answer)
 {
     stack[CarType_Q] = answer;
     if (answer == 1)
@@ -335,7 +306,7 @@ void selectCarType(int answer)
         printf("차량 타입으로 Truck을 선택하셨습니다.\n");
 }
 
-void selectEngine(int answer)
+void CarBuilder::select_engine(int answer)
 {
     stack[Engine_Q] = answer;
     if (answer == 1)
@@ -346,7 +317,7 @@ void selectEngine(int answer)
         printf("WIA 엔진을 선택하셨습니다.\n");
 }
 
-void selectbrakeSystem(int answer)
+void CarBuilder::select_brake_system(int answer)
 {
     stack[brakeSystem_Q] = answer;
     if (answer == 1)
@@ -357,7 +328,7 @@ void selectbrakeSystem(int answer)
         printf("BOSCH 제동장치를 선택하셨습니다.\n");
 }
 
-void selectSteeringSystem(int answer)
+void CarBuilder::select_steering_system(int answer)
 {
     stack[SteeringSystem_Q] = answer;
     if (answer == 1)
@@ -366,7 +337,7 @@ void selectSteeringSystem(int answer)
         printf("MOBIS 조향장치를 선택하셨습니다.\n");
 }
 
-int isValidCheck()
+int CarBuilder::is_valid_assemble()
 {
     if (stack[CarType_Q] == SEDAN && stack[brakeSystem_Q] == CONTINENTAL)
     {
@@ -395,7 +366,7 @@ int isValidCheck()
     return true;
 }
 
-void print_cartype(void)
+void CarBuilder::print_cartype(void)
 {
     if (stack[CarType_Q] == 1)
         printf("Car Type : Sedan\n");
@@ -405,7 +376,7 @@ void print_cartype(void)
         printf("Car Type : Truck\n");
 }
 
-void print_engine(void)
+void CarBuilder::print_engine(void)
 {
     if (stack[Engine_Q] == 1)
         printf("Engine : GM\n");
@@ -415,7 +386,7 @@ void print_engine(void)
         printf("Engine : WIA\n");
 }
 
-void print_brakesystem(void)
+void CarBuilder::print_brakesystem(void)
 {
     if (stack[brakeSystem_Q] == 1)
         printf("Brake System : Mando");
@@ -425,7 +396,7 @@ void print_brakesystem(void)
         printf("Brake System : Bosch\n");
 }
 
-void print_steeringsystem(void)
+void CarBuilder::print_steeringsystem(void)
 {
     if (stack[SteeringSystem_Q] == 1)
         printf("SteeringSystem : Bosch\n");
@@ -433,9 +404,9 @@ void print_steeringsystem(void)
         printf("SteeringSystem : Mobis\n");
 }
 
-void runProducedCar()
+void CarBuilder::run_produced_car()
 {
-    if (isValidCheck() == false)
+    if (is_valid_assemble() == false)
     {
         printf("자동차가 동작되지 않습니다\n");
     }
@@ -457,7 +428,7 @@ void runProducedCar()
     }
 }
 
-void testProducedCar()
+void CarBuilder::test_produced_car()
 {
     if (stack[CarType_Q] == SEDAN && stack[brakeSystem_Q] == CONTINENTAL)
     {
@@ -489,3 +460,45 @@ void testProducedCar()
         printf("자동차 부품 조합 테스트 결과 : PASS\n");
     }
 }
+
+void CarBuilder::run() {
+    std::string input;
+    while (true) {
+        char buf[100];
+        int answer;
+
+        menu_print();
+
+        get_input(buf);
+        if (is_exit(buf))
+            break;
+
+        if (is_valid_number(buf, &answer) == false)
+            continue;
+
+        // 이전으로 돌아가기
+        if (step > CarType_Q && step < Run_Test && answer == 0)
+        {
+            step = static_cast<QuestionType>((int)step - 1);
+            continue;
+        }
+
+        input_handle(answer);
+    }
+}
+
+#ifdef _DEBUG
+#include "gmock/gmock.h"
+int main()
+{
+    testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
+#else
+int main()
+{
+    CarBuilder app;
+    app.run();
+    return 0;
+}
+#endif
